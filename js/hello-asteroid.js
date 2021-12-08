7495c46f020b61181c2c58423ad6b703c82ec1dd
Brice Ernst <brice.ernst1@gmail.com>
2021-12-08 12:01:31 -0600

    adds hello-asteroid

---
+++ js/hello-asteroid.js
@@ -0,0 +1,6 @@
+console.log("Ice cream is one of my favorite foods to eat. " + 'It\'s cool and refreshing on a hot summer day, ' + 'and pairs well with many other desserts.')
+
+var favoriteFood = "ice cream"
+var foodDescription = "cool and refreshing."
+
+console.log("My favorite food is " + favoriteFood + ". It's " + foodDescription)
