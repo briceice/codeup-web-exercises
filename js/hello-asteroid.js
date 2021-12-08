50c5071f914b61e46955f4be44ad3a6e1c449de1
Brice Ernst <brice.ernst1@gmail.com>
2021-12-08 12:30:46 -0600

    asteroid update

--- js/hello-asteroid.js
+++ js/hello-asteroid.js
@@ -1,6 +1,6 @@
 console.log("Ice cream is one of my favorite foods to eat. " + 'It\'s cool and refreshing on a hot summer day, ' + 'and pairs well with many other desserts.')
 
-var favoriteFood = "ice cream"
-var foodDescription = "cool and refreshing."
+var favoriteFood = "\nice cream."
+var foodDescription = "\ncool and refreshing."
 
-console.log("My favorite food is " + favoriteFood + ". It's " + foodDescription)
+console.log("My favorite \n\tfood is " + favoriteFood + "\n\tIt's " + foodDescription)
