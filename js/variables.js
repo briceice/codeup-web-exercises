928065ed1722df540bfda238fef959ce0234cfb4
Brice Ernst <brice.ernst1@gmail.com>
2021-12-09 12:17:14 -0600

    js introduction exercises

---
+++ js/variables.js
@@ -0,0 +1,37 @@
+let a = 1;
+console.log(a)
+let b = a++;
+console.log(b)
+let c = ++a;
+console.log(c)
+
+let d = "hello"
+let e = false
+console.log(d++)
+console.log(e++) // false = 0?
+
+let perplexed;
+console.log(perplexed + 2)
+
+let price = 2.7;
+console.log(price.toFixed(2)) // not 2.70?
+
+// let price2 = "2.7";
+// console.log(price2.toFixed(2))
+
+console.log(isNaN(0))
+
+console.log(!true)
+
+console.log(!!false)
+
+console.log(!!0)
+
+console.log(!!"hello")
+
+console.log(!!"")
+
+console.log(!!"false")
+
+console.log(!!"0")
+
