4c5528408cc639c278f6209ddae95a5cdd8f6637
Brice Ernst <brice.ernst1@gmail.com>
2022-02-03 23:56:20 -0600

    jquery selectors"

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,16 @@
 "use strict";
 
 $(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
+    // alert( 'The DOM has finished loading!' );
+
+    // var contents = $("#extra").html();
+    // alert(contents);
+
+    // $(".codeup").css('border', '1px solid red')
+
+    $('li').css('font-size', '20px');
+    $('h1, p, li').css('background-color', 'yellow')
+    var headerContents = $('h1').html();
+    alert(headerContents);
+
 });
