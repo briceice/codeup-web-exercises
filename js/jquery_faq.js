de4dc40468c5a9cc07df3b443bbd4e4d98e83894
Brice Ernst <brice.ernst1@gmail.com>
2022-02-07 13:47:40 -0600

    jquery traversing

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -10,4 +10,18 @@ $(document).ready(function (){
     $('dt').click(function (){
         $(this).toggleClass('highlight')
     })
+
+    $('#highlight-li').click(function (e) {
+        $('ul').each(function () {
+            $(this).children().last().toggleClass('highlight');
+        })
+    })
+
+    $('h3').click(function (e){
+        $(this).next().children().css('font-weight', 'bold')
+    })
+
+    $('li').click(function (e){
+        $(this).parent().children().first().css('color', 'blue')
+    })
 });
