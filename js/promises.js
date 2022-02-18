fa46e81ee2b32a3568c70135d74be572d021b7fa
Brice Ernst <brice.ernst1@gmail.com>
2022-02-18 12:18:55 -0600

    promises function

--- js/promises.js
+++ js/promises.js
@@ -1,35 +1,38 @@
 "use strict";
 
+function getLastCommit(username) {
+    return fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': `token ${PROMISES_GIT_TOKEN}`}})
+        .then((response) => {
+            return response.json();
+        })
+        .then((jsonData) => {
+            let userRepo = jsonData[0].repo.name
+            return fetch(`https://api.github.com/repos/${userRepo}/commits/main`, {headers: {'Authorization': `token ${PROMISES_GIT_TOKEN}`}})
+                .then((response) => {
+                    return response.json();
+                })
+                .then((jsonData) => {
+                    let lastCommit = jsonData.commit.author.date
+                    return lastCommit
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
 
+getLastCommit('briceice').then(lastCommitDate => console.log(lastCommitDate))
 
-fetch('https://api.github.com/user', {headers: {'Authorization': `token ${PROMISES_GIT_TOKEN}`}})
-    .then((response) => {
-        return response.json();
-    })
-    .then((jsonData) => {
-        console.log(jsonData)
-    })
-    .catch((error) => {
-        console.log(error)})
+function wait(timeout) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(timeout);
+        }, timeout);
+    });
+}
 
-fetch('https://api.github.com/users/briceice/repos', {headers: {'Authorization': `token ${PROMISES_GIT_TOKEN}`}})
-    .then((response) => {
-        return response.json();
-    })
-    .then((jsonData) => {
-        console.log(jsonData)
-        console.log(jsonData[0].pushed_at)
-    })
-    .catch((error) => {
-        console.log(error)})
-
-fetch('https://api.github.com/repos/briceice/codeup-web-exercises/commits/main', {headers: {'Authorization': `token ${PROMISES_GIT_TOKEN}`}})
-    .then((response) => {
-        return response.json();
-    })
-    .then((jsonData) => {
-        console.log(jsonData)
-        console.log(jsonData.commit.author.date)
-    })
-    .catch((error) => {
-        console.log(error)})
+wait(1000).then((ms) => console.log(`You'll see this after ${ms/1000} second`));
+wait(3000).then((ms) => console.log(`You'll see this after ${ms/1000} seconds`));
